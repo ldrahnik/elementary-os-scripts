12113473b6e3fcfb508f9c69f79c79aece739841
Lukáš Drahník <ldrahnik@gmail.com>
2022-04-29 23:45:32 +0200

    Texts

--- microphone.sh
+++ microphone.sh
@@ -2,7 +2,7 @@
 
 if amixer -D pulse set Capture toggle | grep "\[on\]" -q
 then 
-   notify-send -i audio-input-microphone-symbolic " " -a " "
+   notify-send -i audio-input-microphone-symbolic "Unmuted" -a "Microphone"
 else
-   notify-send -i microphone-sensitivity-muted-symbolic " " -a ""
+   notify-send -i microphone-sensitivity-muted-symbolic "Muted" -a "Microphone"
 fi
