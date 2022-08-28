1d0dc03e883616f8ebf23ad05ff51c3ee3ceaca4
Lukáš Drahník <ldrahnik@gmail.com>
2022-08-28 22:38:37 +0200

    Fixed paths inside when is script executed from not the same directory

--- enable_battery_or_ac_mode.sh
+++ enable_battery_or_ac_mode.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 AC=$(cat /sys/class/power_supply/AC0/online)
 
 if [[ $AC = "1" ]]; then
     echo "Enabling ac mode"
-    sh ./ac_mode.sh
+    sh $PARENT_PATH/ac_mode.sh
 else
     echo "Enabling battery mode"
-    sh ./battery_mode.sh
+    sh $PARENT_PATH/battery_mode.sh
 fi
